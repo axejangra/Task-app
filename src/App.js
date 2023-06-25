8d19a08e6014f0c5d74f088ba19f278c40c7db57
sahil <jangras777@gmail.com>
2023-06-25 12:01:47 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -13,31 +13,6 @@ function App() {
     <div>
       <Navbar/>
       <Addtasks/>
-      {/* <form onSubmit={addTask}>
-        <input
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <button type="submit">Add Task</button>
-      </form>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task._id}>
-            <h3>{task.title}</h3>
-            <p>{task.description}</p>
-            <button onClick={() => deleteTask(task._id)}>Delete</button>
-          </li>
-        ))}
-      </ul> */}
-      
     </div>
   );
 }
--- src/components/Addtasks.js
+++ src/components/Addtasks.js
@@ -1,124 +1,153 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from "react";
+const URL = process.env.REACT_APP_BACKEND_URL;
 function Addtasks() {
     const [tasks, setTasks] = useState([]);
-    const [title, setTitle] = useState('');
+    const [title, setTitle] = useState("");
     const [index, setIndex] = useState(null);
-    const [status, setStatus] = useState('Pending');
-    const [description, setDescription] = useState('');
-    const [editingTaskId, setEditingTaskId] = useState(null)
-    
+    const [status, setStatus] = useState(false);
+    const [description, setDescription] = useState("");
+    const [editingTaskId, setEditingTaskId] = useState(null);
 
     useEffect(() => {
         fetchTasks();
-        console.log(tasks);
     }, []);
 
     const fetchTasks = async () => {
-        const response = await fetch('http://localhost:5000/tasks');
+        const response = await fetch(URL, { method: "GET" });
         const data = await response.json();
+        console.log(URL);
         setTasks(data);
     };
 
     const addTask = async (e) => {
-        e.preventDefault()
-        const response = await fetch('http://localhost:5000/tasks', {
-            method: 'POST',
+        e.preventDefault();
+        const response = await fetch(URL, {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify({ title, description}),
+            body: JSON.stringify({ title, description }),
         });
         const data = await response.json();
+        if (editingTaskId) {
+            updateTask(editingTaskId);
 
+            //   const newTasks =  [...tasks]
+            //   newTasks.splice(index,1,data)
+            //   console.log(data);
+            //   setTasks(newTasks)
+            //   setTitle('');
+            //   setDescription('');
+            //   setEditingTaskId(null)
+        } else {
             setTasks([...tasks, data]);
-            setTitle('');
-            setDescription('');  
-        
-        
+            setTitle("");
+            setDescription("");
+        }
     };
 
     const deleteTask = async (id) => {
-        await fetch(`http://localhost:5000/tasks/${id}`, {
-            method: 'DELETE',
+        await fetch(URL+id, {
+            method: "DELETE",
         });
         setTasks(tasks.filter((task) => task._id !== id));
     };
-    const updateTask = async () => {
-        const response = await fetch(`http://localhost:5000/tasks/${editingTaskId}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ title, description }),
+
+    const updateTask = async (id) => {
+        const response = await fetch(URL+id, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ title, description }),
         });
         await response.json();
-        setTasks(
-          tasks.map((task) =>
-            task._id === editingTaskId ? { ...task, title, description } : task
-          )
-        );
-        setTitle('');
-        setDescription('');
-        setEditingTaskId(null);
-      };
 
-    const startEditing = (id,index) => {
+        setTasks(
+            tasks.map((task) =>
+                task._id === editingTaskId ? { ...task, title, description } : task
+            )
+        );
+        setTitle("");
+        setDescription("");
+        setEditingTaskId(null);
+    };
+
+    const startEditing = (id) => {
+        console.log(id);
         const taskToEdit = tasks.find((task) => task._id === id);
         if (taskToEdit) {
-          setTitle(taskToEdit.title);
-          setDescription(taskToEdit.description);
-          setEditingTaskId(id);
-          setIndex(index)
+            setTitle(taskToEdit.title);
+            setDescription(taskToEdit.description);
+            setEditingTaskId(id);
         }
-      };
+    };
     return (
         <>
-
-            <section className="vh-100" style={{ backgroundColor: "#eee" }}>
+            <section className="" style={{ backgroundColor: "#eee" }}>
                 <div className="container py-5 h-100">
                     <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col col-lg-9 col-xl-7">
+                        <div className="col">
                             <div className="card rounded-3">
                                 <div className="card-body p-4">
                                     <h3 className="text-center my-3 pb-3">My Task Manager App</h3>
-                                    <form onSubmit={addTask} className=" row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2" >
+                                    <form
+                                        onSubmit={addTask}
+                                        className=" row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2"
+                                    >
                                         <div className=" col-12">
                                             <div className="mb-4 form-outline">
-                                                <input type="text" required={true} placeholder="Title"
+                                                <input
+                                                    type="text"
+                                                    required={true}
+                                                    placeholder="Title"
                                                     value={title}
-                                                    onChange={(e) => setTitle(e.target.value)} id="form1" className="form-control" />
-
+                                                    onChange={(e) => setTitle(e.target.value)}
+                                                    id="form1"
+                                                    className="form-control"
+                                                />
                                             </div>
                                             <div className="form-outline">
-                                                <input type="text" required={true} placeholder="Description"
+                                                <input
+                                                    type="text"
+                                                    required={true}
+                                                    placeholder="Description"
                                                     value={description}
-                                                    onChange={(e) => setDescription(e.target.value)} id="form2" className="form-control" />
-
+                                                    onChange={(e) => setDescription(e.target.value)}
+                                                    id="form2"
+                                                    className="form-control"
+                                                />
                                             </div>
-
                                         </div>
                                         <div className="text-center mt-4 me-2 col-12">
-                                            <button type="submit" onClick={(e) => e.preventDefault} className="btn btn-primary">
-                                            {editingTaskId ? 'Update Task' : 'Add Task'}
+                                            <button
+                                                type="submit"
+                                                onClick={(e) => e.preventDefault}
+                                                className="btn btn-primary"
+                                            >
+                                                {editingTaskId ? "Update Task" : "Add Task"}
                                             </button>
-                                            <button type='reset' onClick={() => {
-                                                setTitle('');
-                                                setDescription('');
-                                                setEditingTaskId(null)
-                                            }} className="btn btn-warning ms-2">
+                                            <button
+                                                type="reset"
+                                                onClick={() => {
+                                                    setTitle("");
+                                                    setDescription("");
+                                                    setEditingTaskId(null);
+                                                }}
+                                                className="btn btn-warning ms-2"
+                                            >
                                                 Reset
                                             </button>
                                         </div>
-
                                     </form>
+                                   
+
                                     <table className="table mb-4">
                                         <thead>
                                             <tr>
                                                 <th scope="col">No.</th>
                                                 <th scope="col">Todo item</th>
                                                 <th scope="col">Description</th>
-                                                <th scope="col">Status</th>
                                                 <th scope="col">Actions</th>
                                                 <th scope="col">Change Status</th>
                                                 <th scope="col">Edit</th>
@@ -130,27 +159,34 @@ function Addtasks() {
                                                     <th scope="row">{index + 1}</th>
                                                     <td>{task.title}</td>
                                                     <td>{task.description}</td>
-                                                    <td>{task.status}</td>
                                                     <td>
-                                                        <button onClick={() => deleteTask(task._id)} className="btn btn-danger">
+                                                        <button
+                                                            onClick={() => deleteTask(task._id)}
+                                                            className="btn btn-danger"
+                                                        >
                                                             Delete
                                                         </button>
                                                     </td>
                                                     <td>
-                                                        <button type="submit" onClick={() => { setStatus('Finished') }} className="btn btn-success ms-1">
-                                                            Finished
+                                                        <button
+                                                            onClick={() => {
+                                                                setStatus(!status);
+                                                            }}
+                                                            className="btn btn-success ms-1"
+                                                        >
+                                                            {status === false ? "Pending" : "Finished"}
                                                         </button>
                                                     </td>
                                                     <td>
-                                                        <button type="submit" onClick={()=>startEditing(task._id,index)}  className="btn btn-success ms-1">
+                                                        <button
+                                                            onClick={() => startEditing(task._id)}
+                                                            className="btn btn-success ms-1"
+                                                        >
                                                             Edit
                                                         </button>
                                                     </td>
                                                 </tr>
-
-                                            ))
-                                            }
-
+                                            ))}
                                         </tbody>
                                     </table>
                                 </div>
@@ -159,9 +195,8 @@ function Addtasks() {
                     </div>
                 </div>
             </section>
-
         </>
-    )
+    );
 }
 
 export default Addtasks;
